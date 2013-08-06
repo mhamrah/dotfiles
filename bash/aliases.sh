de42d4fc6f20567c59bfc4493e0df9f1e6d012e7
mhamrah <mhamrah@gettyimages.com>
2013-08-06 11:07:43 -0400

    updating aliases, submodules

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,3 @@
-alias dnsflush="dscacheutil -flushcache"
 alias mlh-cloud="ssh -i ~/.ssh/aws-mlh.pem ec2-user@23.23.186.107"
 alias vi="vim"
 alias gka="gitk --all"
@@ -8,6 +7,53 @@ alias ctags="`brew --prefix`/bin/ctags"
 
 alias discourse="ssh -i ~/.ssh/discourse.pem ubuntu@discourse.gtty.in"
 
-alias dg="cd ~/dev/gyi"
-alias dp="cd ~/dev/p"
-alias di="cd ~/dev/gh"
+alias dsa="cd ~/dev/gyi/dsa"
+alias gh="cd ~/dev/gh"
+alias pd="cd ~/dev/p"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+
+alias d="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias g="git"
+alias v="mvim ."
+alias h="history"
+alias j="jobs"
+
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+else # OS X `ls`
+	colorflag="-G"
+fi
+
+# List all files colorized in long format
+#alias l="ls -l ${colorflag}"
+
+# List all files colorized in long format, including dot files
+#alias la="ls -la ${colorflag}"
+
+#alias ls="command ls ${colorflag}"
+#export LS_COLORS='di=93'
+#export LS_COLORS='no=00:fi=00:di=92;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+
+# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='sudo softwareupdate -i -a; brew update; brew upgrade; brew cleanup; npm update npm -g; npm update -g; sudo gem update'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en1"
+alias ips="ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'"
+
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Recursively delete `.DS_Store` files
+alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
