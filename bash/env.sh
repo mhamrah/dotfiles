4b1f6d5359485a16cc1e4db9b27e9637661c1031
mhamrah <mhamrah@gettyimages.com>
2013-08-30 11:46:23 -0400

    Adding CFN variables

--- bash/env.sh
+++ bash/env.sh
@@ -9,3 +9,10 @@ export TERM='xterm-256color'
 # No duplicates in history.
 export HISTCONTROL=ignoredups
 
+export AWS_CREDENTIAL_FILE=~/.ssh/gyi-aws-cred
+export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
+export JAVA_HOME="$(/usr/libexec/java_home)"
+export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ssh/gyi-production.pem | /usr/bin/head -1)"
+
+source ~/.ssh/gyi-aws-cred.env
