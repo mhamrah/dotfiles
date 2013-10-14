e2a43d049a2350f2a3ea3c32f776258364f0f139
Michael Hamrah <m@hamrah.com>
2013-10-14 10:11:33 -0400

    scalariform

--- bash/env.sh
+++ bash/env.sh
@@ -15,4 +15,7 @@ export JAVA_HOME="$(/usr/libexec/java_home)"
 export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
 export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ssh/gyi-production.pem | /usr/bin/head -1)"
 
-source ~/.ssh/gyi-aws-cred.env
+source ~/.ssh/g2-aws-cred.env
+
+export AWS_CLOUDFORMATION_URL=https://cloudformation.us-west-2.amazonaws.com
+export EC2_URL=https://ec2.us-west-2.amazonaws.com
