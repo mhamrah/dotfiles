43d4f8611095e7a0ba07de287db5a050e364ecc0
Michael Hamrah <m@hamrah.com>
2013-10-21 15:34:04 -0400

    clevertab

--- bash/env.sh
+++ bash/env.sh
@@ -7,15 +7,15 @@ export PGDATABASE=postgres
 export TERM='xterm-256color'
 
 # No duplicates in history.
-export HISTCONTROL=ignoredups
+#export HISTCONTROL=ignoredups
 
-export AWS_CREDENTIAL_FILE=~/.ssh/gyi-aws-cred
-export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
-export JAVA_HOME="$(/usr/libexec/java_home)"
-export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
-export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ssh/gyi-production.pem | /usr/bin/head -1)"
+#export AWS_CREDENTIAL_FILE=~/.ssh/gyi-aws-cred
+#export AWS_CLOUDFORMATION_HOME="/usr/local/Library/LinkedKegs/aws-cfn-tools/jars"
+#export JAVA_HOME="$(/usr/libexec/java_home)"
+#export EC2_HOME="/usr/local/Library/LinkedKegs/ec2-api-tools/jars"
+#export EC2_PRIVATE_KEY="$(/bin/ls "$HOME"/.ssh/gyi-production.pem | /usr/bin/head -1)"
 
-source ~/.ssh/g2-aws-cred.env
+#source ~/.ssh/g2-aws-cred.env
 
-export AWS_CLOUDFORMATION_URL=https://cloudformation.us-west-2.amazonaws.com
-export EC2_URL=https://ec2.us-west-2.amazonaws.com
+#export AWS_CLOUDFORMATION_URL=https://cloudformation.us-west-2.amazonaws.com
+#export EC2_URL=https://ec2.us-west-2.amazonaws.com
