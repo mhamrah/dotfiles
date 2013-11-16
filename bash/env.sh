1c87bba22c80adc88c435b64208bba08c65598bc
Michael Hamrah <m@hamrah.com>
2013-11-16 09:08:38 -0500

    updating path

--- bash/env.sh
+++ bash/env.sh
@@ -1,11 +1,12 @@
 export TM_RUBY=/usr/local/bin/ruby
-export GOROOT=/usr/local/Cellar/go/1.0.3
-export GOPATH=~/dev/go
 export PGDATABASE=postgres
+
+export GOPATH=~/dev/go
+export PATH="$PATH:$GOPATH/bin"
 
 # Colors in Vim.
 export TERM='xterm-256color'
-
+source ~/Dropbox/Trunk/env.sh
 # No duplicates in history.
 #export HISTCONTROL=ignoredups
 
