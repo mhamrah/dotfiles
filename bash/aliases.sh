fc36dae70ca2648443ad83b0d9dd12e134b99eab
Michael Hamrah <m@hamrah.com>
2014-03-19 23:30:05 -0400

    updated tmux alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -28,7 +28,7 @@ alias j="jobs"
 alias al="mvim ~/.dotfiles/bash/aliases.sh"
 alias gr="grep -i"
 alias s="source ~/.bash_profile"
-alias ta="tmux attach || tmux new"
+alias ta="tmux new-session -t default || tmux new-session -s default"
 
 # Copy my public key to the pasteboard
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | printf '=> Public key copied to pasteboard.\n'"
