27c5a21b97ebd1b7238700082e0b86979fa98729
Michael Hamrah <m@hamrah.com>
2015-01-02 10:17:24 -0500

    updated path

--- bash/path.sh
+++ bash/path.sh
@@ -1,12 +1,7 @@
 #Local bin
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 
-#Mysql
-export PATH=/usr/local/mysql/bin:$PATH
-
-#npm
-export PATH=/usr/local/share/npm/bin:$PATH
+#python
+export PATH=~/Library/Python/2.7/bin:$PATH
 
 export PATH=~/Dropbox/bin:$PATH
-export PATH=~/mat:$PATH
-export PATH=~/bin/spark/bin:~/bin/spark/sbin:$PATH
