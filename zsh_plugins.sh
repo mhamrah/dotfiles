94341fc1457df1604ad99a98ee917a926bcfd54c
Mike Hamrah <m@hamrah.com>
2018-03-09 21:09:01 -0500

    checkpoint

---
+++ zsh_plugins.sh
@@ -0,0 +1,9 @@
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-caarlos0-SLASH-ports/ports.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-chrissicool-SLASH-zsh-256color/zsh-256color.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sei40kr-SLASH-zsh-tmux-rename/zsh-tmux-rename.plugin.zsh
