ccca5112e16fcdfc04e007cbad69705c1dc6f411
Mike Hamrah <m@hamrah.com>
2018-03-10 20:20:06 -0500

    better time

--- bin/status_right.sh
+++ bin/status_right.sh
@@ -4,9 +4,9 @@
 WIDTH=${1}
 
 if [[ $WIDTH -ge 160 ]]; then
-    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] %Y-%m-%d  %H:%M #[fg=colour252,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour252] #h"
+    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] $(date +%y.%m.%d)  $(date +%H:%M) #[fg=colour252,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour252] #h"
 elif [[ $WIDTH -ge 120 ]]; then
-    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] %H:%M #[fg=colour252,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour252] #h"
+    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] $(date +%H:%M) #[fg=colour252,bg=colour236,nobold,nounderscore,noitalics]#[fg=colour235,bg=colour252] #h"
 else
-    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] %H:%M"
+    echo -n "#[fg=colour236,bg=colour234,nobold,nounderscore,noitalics]#[fg=colour247,bg=colour236] $(date +%H:%M)"
 fi
