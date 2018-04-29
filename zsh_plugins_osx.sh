7db668c98d77caf535e09c5ccb952690f59e13d7
Mike Hamrah <m@hamrah.com>
2018-04-29 13:22:22 -0400

    better osx and linux

---
+++ zsh_plugins_osx.sh
@@ -0,0 +1,11 @@
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mhamrah-SLASH-pure/pure.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-chrissicool-SLASH-zsh-256color/zsh-256color.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/tmux/tmux.plugin.zsh
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/autojump/autojump.plugin.zsh
+
+source /Users/mhamrah/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/docker-compose/docker-compose.plugin.zsh
