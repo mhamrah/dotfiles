fb474f7f26055c79aad18e995c2bbd64731456f7
Mike Hamrah <m@hamrah.com>
2018-08-02 23:07:23 -0400

    wsl setup for tmux; windows aliases

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,5 @@
 Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
 Set-Alias dc docker-compose
 Set-Alias g git
+Set-PSReadlineOption -TokenKind Parameter -Foregroundcolor Cyan
+Set-PSReadlineOption -TokenKind Operator -Foregroundcolor White
