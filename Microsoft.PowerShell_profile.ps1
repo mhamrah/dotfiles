1d6152d9485b3c8c2fc2aedab79599aa8bfe06d5
Mike Hamrah <m@hamrah.com>
2018-08-02 21:37:53 -0400

    aliases

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-9bda399\src\posh-git.psd1'
+Set-Alias dc docker-compose
+Set-Alias g git
---
+++ aliases.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+:: add to HKEY_CURRENT_USER\Software\Microsoft\Command Processor
+
+DOSKEY ls=dir /B
+DOSKEY dc=docker-compose
+DOSKEY g=git
