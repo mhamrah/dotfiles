7f5c148cbe800ef566b695d32f1193285a9f1061
Mike Hamrah <m@hamrah.com>
2018-08-20 21:12:25 -0400

    updates

---
+++ bin/kubectx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+KUBESYMBOL=$'\u2388'
+
+echo -n '#[fg=colour237,bg=colour234]'
+
+echo -n "#[fg=colour140,bg=colour237] $KUBESYMBOL $(kubectl config view --minify --output 'jsonpath={.current-context}'):#[fg=colour145]$(kubectl config view --minify --output 'jsonpath={..namespace}')"
